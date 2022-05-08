8d779f270673a884d0de64c3314a2a1609c7971e
Aitcheffoh <hecfpasaje@misena.edu.co>
2022-05-08 07:59:54 -0500

    Initial version of YourPcStore generated by generator-jhipster@7.8.1

---
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'yps-footer',
+  templateUrl: './footer.component.html',
+})
+export class FooterComponent {}
---
+++ src/test/java/com/edu/sena/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.edu.sena;
+
+import com.edu.sena.YourPcStoreApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = YourPcStoreApp.class)
+public @interface IntegrationTest {
+}
